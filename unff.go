c6a488ddc2e49dd215ca872a2a1cfe5e8356ce97
Dan Mundy <drm.mundy@gmail.com>
2014-01-18 01:19:45 -0500

    Adds command line arguments that do nothing
    Temporarily comments anaconda so it runs without complaining about
    unused variables

--- unff.go
+++ unff.go
@@ -6,8 +6,21 @@ package main
 
 import (
   "fmt"
-  "github.com/chimeracoder/anaconda"
+  "flag"
+  // "github.com/chimeracoder/anaconda"
 )
 
+// Options
+var inactive, tooactive, interactive *bool
+
 func main() {
+  getFlags()
+  fmt.Printf("inactive: %t\ntooactive: %t\ninteractive: %t\n", *inactive, *tooactive, *interactive)
+}
+
+func getFlags() {
+  inactive = flag.Bool("inactive", false, "Unfollow inactive accounts")
+  tooactive = flag.Bool("tooactive", false, "Unfollow too-active accounts")
+  interactive = flag.Bool("interactive", false, "Select which accounts to unfollow")
+  flag.Parse()
 }
